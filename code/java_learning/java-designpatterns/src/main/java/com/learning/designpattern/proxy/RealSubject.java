c36c42210f327a895a66d42437bd8f82401cb7ee
morning <weiyuntao@126.com>
2020-09-20 19:33:54 +0800

    proxy design

---
+++ code/java_learning/java-designpatterns/src/main/java/com/learning/designpattern/proxy/RealSubject.java
@@ -0,0 +1,20 @@
+package com.learning.designpattern.proxy;
+
+/**
+ *  真实业务主题
+ * @author morning
+ * @date 2020/9/20 19:23
+ * @since 0.0.1
+ */
+public class RealSubject implements ISubject{
+
+    /**
+     *  真实的业务处理
+     * @author morning
+     * @date 2020/9/20 19:23
+     * @since 0.0.1
+     */
+    public void request() {
+        System.out.println("真实的业务处理......");
+    }
+}
